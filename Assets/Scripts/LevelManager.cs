041e1b13427bdf840af237b617777823dde102fe
Nick Brander <allan.firewind@gmail.com>
2017-04-27 11:15:49 -0500

    Added 'Game' components and menu items, as well as 'Quit' function

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,5 +5,11 @@ public class LevelManager : MonoBehaviour {
 
 	public void LoadLevel (string name) {
 		Debug.Log("Level load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest () {
+		Debug.Log ("Quit Game");
+		Application.Quit ();
 	}
 }
